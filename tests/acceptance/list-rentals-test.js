973c48a37e5d309c485fcc9dc0b4f2aab70bfe92
John Lewis <lewisjnerd@gmail.com>
2017-01-19 17:03:06 -0500

    inital commit

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,55 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should redirect to rentals route', (assert) => {
+  visit('/');
+  andThen(() => {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should list available rentals.', assert => {
+  visit('/');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+test('should link to information about the company.', (assert) => {
+  visit('/');
+  click('a:contains("About")');
+  andThen(() => {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+
+test('should link to contact information.', (assert) => {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(() => {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+
+test('should filter the list of rentals by city.', (assert) => {
+  visit('/');
+  fillIn('.list-filter', 'seattle');
+  keyEvent('.list-filter', 'Keyup', 69);
+  andThen(() => {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .locations:contains("Seattle")'), 1,
+      'should show 1 lisitng with seattle');
+  });
+});
+
+test('should show details for a specific rental', (assert) => {
+  visit('/rentals');
+  click('a:contains("Grand Old Mansion")');
+  andThen(() => {
+    assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
+    assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+    assert.equal(find('.description').length, 1, 'should list a description of the property');
+  });
+});
